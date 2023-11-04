9a61b7a02755d558305597965c28ef531dac17e4
Erik <>
2023-11-04 02:02:59 -0500

    fix gulpfile bug, fix sass imports

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function compileSass() {
-    return gulp.src('./static/scss/**/*.scss').pipe(sass()).pipe(gulp.dest('./static/cscc'));
+    return gulp.src('./static/scss/**/*.scss').pipe(sass()).pipe(gulp.dest('./static/css'));
 }
 
-gulp.task('sass', compileSass());
+function watchSass() {
+    return gulp.watch('./static/scss/**/*.scss', compileSass);
+}
 
-gulp.task('watch', compileSass());
+gulp.task('sass', compileSass);
+
+gulp.task('watch', watchSass);
